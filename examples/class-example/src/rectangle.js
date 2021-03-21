0d1049f7e052931cb69c3391b9525b706c342aae
OscarMoreiraEstevez <alu0101209067@ull.edu.es>
2021-03-21 15:39:28 +0000

    example class test

---
+++ examples/class-example/src/rectangle.js
@@ -0,0 +1,18 @@
+'use strict'
+
+class Rectangle {
+  constructor (height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  
+  get area() {
+    return this.calcArea();
+  }
+  
+  calcArea () {
+    return this.height * this.width;
+  }
+}
+
+module.exports = {Rectangle}
